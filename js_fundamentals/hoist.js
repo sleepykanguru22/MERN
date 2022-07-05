ef784f5b4273aca873823c3b5f494eb8ff42a2d5
sleepykanguru22 <70289696+sleepykanguru22@users.noreply.github.com>
2022-07-05 14:12:43 -0700

    Create hoist.js

---
+++ js_fundamentals/hoist.js
@@ -0,0 +1,57 @@
+console.log(hello);
+var hello = "world";
+
+// var needle = "haystack";
+// test();
+// function test() {
+//   var needle = "magnet";
+//   console.log(needle);
+// }
+
+// var brendan = "super cool";
+// function print() {
+//   brendan = "only okay";
+//   console.log(brendan);
+// }
+// console.log(brendan);
+
+// var food = "chicken";
+// console.log(food);
+// eat();
+// function eat() {
+//   food = "half-chicken";
+//   console.log(food);
+//   var food = "gone";
+// }
+
+// mean();
+// console.log(food);
+// var mean = function () {
+//   food = "chicken";
+//   console.log(food);
+//   var food = "fish";
+//   console.log(food);
+// };
+// console.log(food);
+
+// console.log(genre);
+// var genre = "disco";
+// rewind();
+// function rewind() {
+//   genre = "rock";
+//   console.log(genre);
+//   var genre = "r&b";
+//   console.log(genre);
+// }
+// console.log(genre);
+
+// dojo = "san jose";
+// console.log(dojo);
+// learn();
+// function learn() {
+//   dojo = "seattle";
+//   console.log(dojo);
+//   var dojo = "burbank";
+//   console.log(dojo);
+// }
+// console.log(dojo);
