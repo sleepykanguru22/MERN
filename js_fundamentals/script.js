dd6195f5c16663328bed7ec62f359b55c504de45
sleepykanguru22 <70289696+sleepykanguru22@users.noreply.github.com>
2022-07-06 12:44:33 -0700

    Update script.js

--- js_fundamentals/script.js
+++ js_fundamentals/script.js
@@ -1,23 +1,27 @@
-class ExampleButton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { isCLicked: false };
-  }
-  render() {
-    if (this.state.isCLicked) {
-      return "You clicked this";
-    }
-    return React.createElement(
-      "button",
-      { onClick: () => this.setState({ isCLicked: true }) },
-      "Click me!"
-    );
-  }
-}
+// class ExampleButton extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { isCLicked: false };
+//   }
+//   render() {
+//     if (this.state.isCLicked) {
+//       return "You clicked this";
+//     }
+//     return React.createElement(
+//       "button",
+//       { onClick: () => this.setState({ isCLicked: true }) },
+//       "Click me!"
+//     );
+//   }
+// }
 
 // select root div
-const domContainer = document.querySelector("#root");
+// const domContainer = document.querySelector("#root");
 // create the root of react dom
-const root = ReactDOM.createRoot(domContainer);
+// const root = ReactDOM.createRoot(domContainer);
 // render what we see
-root.render(React.createElement(ExampleButton));
+// root.render(React.createElement(ExampleButton));
+
+const domContainer = document.querySelector("#root");
+const root = ReactDOM.createRoot(domContainer);
+root.render(React.createElement("h1", {}, "Our First React page has rendered"));
