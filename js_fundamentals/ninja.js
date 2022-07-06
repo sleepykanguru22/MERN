9971404838cdb6b236e3de18f8ebd73014d483e6
sleepykanguru22 <70289696+sleepykanguru22@users.noreply.github.com>
2022-07-05 18:38:18 -0700

    Create ninja.js

---
+++ js_fundamentals/ninja.js
@@ -0,0 +1,23 @@
+class Ninja {
+  constructor(name) {
+    this.name = name;
+    this.health = 3;
+    this.speed = 3;
+    this.strength = 3;
+  }
+
+  sayName = () => console.log(this.name);
+
+  showStats = () =>
+    console.log(
+      `health:${this.health} speed:${this.speed} strength:${this.strength}`
+    );
+  drinkSake() {
+    this.health += 10;
+  }
+}
+
+const ninja1 = new Ninja("Hyabusa");
+ninja1.sayName();
+ninja1.drinkSake();
+ninja1.showStats();
