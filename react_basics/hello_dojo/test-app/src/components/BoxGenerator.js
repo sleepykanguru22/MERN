f2cefc7803b4b5b08a9705bd0ca1c487cea6e5f9
sleepykanguru22 <70289696+sleepykanguru22@users.noreply.github.com>
2022-07-12 20:20:55 -0700

    Update BoxGenerator.js

--- react_basics/hello_dojo/test-app/src/components/BoxGenerator.js
+++ react_basics/hello_dojo/test-app/src/components/BoxGenerator.js
@@ -1,40 +1,24 @@
 import React, { useState } from "react";
 
 const BoxGenerator = (props) => {
-  const { boxPropertyArr, setBoxPropertyArr } = props;
-
   const [color, setColor] = useState("");
-  const [size, setSize] = useState(50);
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    setBoxPropertyArr([
-      ...boxPropertyArr,
-      {
-        color: color,
-        size: size + "px"
-      }
-    ]);
-    setColor("");
-    setSize(50);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    props.onNewBox(color);
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit} style={{ margin: "20px" }}>
-        <div>
-          <label>Color</label>
-          <input
-            type="text"
-            name="color"
-            value={color}
-            onChange={(e) => setColor(e.target.value)}
-          />
-        </div>
-        <button>Add</button>
-      </form>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <h1>Create Box</h1>
+      <input
+        type="text"
+        placeholder="Pick a color"
+        onChange={(e) => setColor(e.target.value)}
+        value={color}
+      ></input>
+      <input type="submit" value="Add" />
+    </form>
   );
 };
 
